c1f28652422b463b96059447529a61efd3b3a4a6
Herdian Afrody <herdianafrody@gmail.com>
2024-05-07 12:48:02 +0700

    First commit on role-module

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const router = require("express").Router();
 const port = 3000;
 
-console.log(app.use(router));
+app.use(router);
 
 app.listen(port, () => {
   console.log("App listening on port: " + port);
